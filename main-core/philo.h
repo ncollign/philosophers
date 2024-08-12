f0483f878517a400fbd2c2520cd8a0719c56367a
ncollign <ncollign@student.42luxembourg.lu>
2024-08-12 15:55:35 +0200

    m

--- main-core/philo.h
+++ main-core/philo.h
@@ -6,7 +6,7 @@
 /*   By: ncollign <ncollign@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:19:08 by ncollign          #+#    #+#             */
-/*   Updated: 2024/08/12 15:35:10 by ncollign         ###   ########.fr       */
+/*   Updated: 2024/08/12 15:43:23 by ncollign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,24 @@
 
 typedef struct s_philo
 {
-	pthread_t		thread;
+	pthread_t		thread_id;
 	int				id;
-	int				eating;
-	int				meals_eaten;
-	size_t			last_meal;
-	size_t			time_to_die;
-	size_t			time_to_eat;
-	size_t			time_to_sleep;
-	size_t			start_time;
-	int				num_of_philos;
-	int				num_times_to_eat;
-	int				*dead;
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*write_lock;
-	pthread_mutex_t	*dead_lock;
-	pthread_mutex_t	*meal_lock;
+	int				nb_eat;
+	int				dead;
+	int				r_fork_id;
+	int				l_fork_id;
+	t_rules			*rules;
 }					t_philo;
 
 typedef struct s_rules
 {
-	t_philo			*philos;
-	int				nb_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				nb_time_eat;
+	t_philo				*philos;
+	pthread_mutex_t		*forks;
+	int					nb_philo;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					nb_time_eat;
 }					t_rules;
 
 void	init_philo(void);
