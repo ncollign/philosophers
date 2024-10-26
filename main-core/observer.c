65dd5ed9bd135ccb6fe3e3a1f6a9d90c08590269
Collignon Nicolas <nicolas.collignon7@gmail.com>
2024-10-26 20:59:14 +0200

    m

--- main-core/observer.c
+++ main-core/observer.c
@@ -6,15 +6,22 @@
 /*   By: ncollign <ncollign@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 12:17:18 by ncollign          #+#    #+#             */
-/*   Updated: 2024/10/21 14:23:28 by ncollign         ###   ########.fr       */
+/*   Updated: 2024/10/26 20:58:29 by ncollign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	exit_simulation(t_rules rules)
+{
+	printf("Hello %d\n", rules.nb_philo);
+}
+
+
 void	*observer(void *arg)
 {
     t_rules	*rules = (t_rules *)arg;
+	long	time_since_last_meal;
     int		i;
 
     while (1)
@@ -26,23 +33,24 @@ void	*observer(void *arg)
             if (rules->simulation_running == 0)
             {
                 pthread_mutex_unlock(&rules->sim_mutex);
-                return (NULL);
+                exit_simulation(*rules);
+				return(NULL);
             }
             pthread_mutex_unlock(&rules->sim_mutex);
 
-            long time_since_last_meal = get_current_time(rules) - rules->philos[i].last_meal;
+            time_since_last_meal = get_current_time(rules) - rules->philos[i].last_meal;
             if (time_since_last_meal > rules->time_to_die)
             {
                 pthread_mutex_lock(&rules->sim_mutex);
                 rules->simulation_running = 0;
                 pthread_mutex_unlock(&rules->sim_mutex);
-
                 printf("%ld %d died\n", get_current_time(rules), rules->philos[i].id);
-                return (NULL);
+				exit_simulation(*rules);
+				return(NULL);
             }
             i++;
         }
-        usleep(1000); // Petite pause pour éviter de surcharger le CPU
+        usleep(1000);
     }
-    return (NULL);
+	return(NULL);
 }
